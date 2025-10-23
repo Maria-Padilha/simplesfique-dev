570477dcfc067307e51513d3ffb5e66b81d375c8
devvstmagosso <fmagossovst@gmail.com>
2025-10-23 11:41:35 -0400

    deixando a home limpa [FIX]

--- src/views/pages/HomeView.vue
+++ src/views/pages/HomeView.vue
@@ -1,13 +1,9 @@
 <template>
   <main class="my-12 mx-10">
-    <h1 class="text-2xl mb-5">Modais já disponíveis</h1>
-
-    <v-btn @click="modal = !modal" variant="tonal" color="var(--text-color-laranja)">Modal País</v-btn>
+   Home
   </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 
-const modal = ref(false);
 </script>
